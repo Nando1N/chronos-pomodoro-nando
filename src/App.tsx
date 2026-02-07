f31a4838ba76ca8e6384de2b30d744b084988521
Hernandes <hernnades.pires10@gmail.com>
2026-02-07 00:04:03 -0300

    adicionado disable no formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Container } from "./components/Container";
 import { CountDown } from "./components/Countdown";
+import { DefaultInput } from "./components/DefaultInput";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 
@@ -18,7 +19,34 @@ export function App() {
       </Container>
 
       <Container>
-        <CountDown/>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput
+              labelText='task'
+              id='meuInput'
+              type='number'
+              placeholder='Digite algo'
+              disabled
+            />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit, amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
